6ecfb267c21be901bc9fb81f8b12b278fb053c58
akcortes <92767118+akcortes@users.noreply.github.com>
2022-01-17 10:54:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,9 +10,4 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-[theme]\n\
-backgroundColor = \"#cbe8e8"\n\
-secondaryBackgroundColor=\"#3231b1"\n\
-textColor=\"#230871"\n\
-font=\"serif"\n\
 " > ~/.streamlit/config.toml
